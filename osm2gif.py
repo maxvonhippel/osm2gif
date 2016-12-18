8794111ecebc65553719d7c3f238c01d51609faf
Max von Hippel <maxvonhippel1996@gmail.com>
2016-12-17 23:33:42 -0700

    redid the csv etc, basically rewrote the script, testing now

--- osm2gif.py
+++ osm2gif.py
@@ -2,9 +2,10 @@
 import linecache
 import sys
 import dateutil.parser
-import osmium
 from staticmap import StaticMap, CircleMarker, Polygon
 import imageio
+import csv
+from StringIO import StringIO
 
 # http://stackoverflow.com/a/20264059/1586231
 def PrintException():
@@ -16,17 +17,17 @@ def PrintException():
     line = linecache.getline(filename, lineno, f.f_globals)
     print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
 
-class Day()
+class Day():
 	# init a day
 	def __init__(self):
 		# dateutil.parser.parse(day).timestamp()
 		self.nodes = []
 		self.ways = []
 	# add a node
-	def add_node_to_day(node):
+	def add_node_to_day(self, node):
 		self.nodes.append(node)
 	# add a way
-	def add_way_to_day(way):
+	def add_way_to_day(self, way):
 		self.ways.append(way)
 
 # to hold all our days
@@ -46,38 +47,36 @@ def add_way(items, timestamp):
 		days[timestamp] = Day()
 	days[timestamp].add_way_to_day(polygon)
 
-# parse the nodes, relations, and ways
-class CounterHandler(osmium.SimpleHandler):
-	# the init variables of the Counter Handler
-    def __init__(self):
-    	osmium.SimpleHandler.__init__(self)
 
-    def node(self, n):
-    	# handle a node
-    	add_node(n.location.lon, n.location.lat, n.timestamp)
-
-    def way(self, w):
-    	# handle a way
-    	add_way(w.nodes, w.timestamp)
-
-# handle command line args
-# we take the following format:
-# python osm2gif.py filename outname
-if len(sys.argv) != 6:
-	print("Please try again with format: python osm2gif.py file.osm outname width height zoom")
-else:
+# for OSM (.osm, .osh, .pbf) files
+def read_osm(file, width, height, zoom, video_name):
 	try:
+		# imports
+		import osmium
+		# class to parse the nodes, relations, and ways
+		class CounterHandler(osmium.SimpleHandler):
+			# the init variables of the Counter Handler
+		    def __init__(self):
+		    	osmium.SimpleHandler.__init__(self)
+
+		    def node(self, n):
+		    	# handle a node
+		    	add_node(n.location.lon, n.location.lat, n.timestamp)
+
+		    def way(self, w):
+		    	# handle a way
+		    	add_way(w.nodes, w.timestamp)
+		# instantiate one    	
 		map_handler = CounterHandler()
-		map_handler.apply_file(sys.argv[1])
+		map_handler.apply_file(file)
 		# on complete, render frames into a video
 		# http://stackoverflow.com/a/35943809/1586231
-		video_name = sys.argv[3]
 		if not video_name.str.endswith('.gif'):
 			video_name += '.gif'
 		with imageio.get_writer(video_name, mode='I') as writer:
 			for stamp in days:
 				# make the map
-				_map = StaticMap(sys.argv[3], sys.argv[4], url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
+				_map = StaticMap(width, height, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
 				# add the nodes
 				for node in days[stamp].nodes:
 					_map.add_marker(node)
@@ -85,7 +84,7 @@ else:
 				for way in days[stamp].ways:
 					_map.add_polygon(way)
 				# render
-				_img = _map.render(zoom=sys.argv[5])
+				_img = _map.render(zoom=zoom)
 				# save
 				_name = stamp + ".png"
 				_img.save(_name)
@@ -96,3 +95,54 @@ else:
 			print("all done.")
 	except:
 		PrintException()
+
+# for .csv files
+# format: id,longitude,latitude,"{[Name:timestamp], ... }"
+def read_csv(file, width, height, zoom, video_name):
+	parsd = 0
+	mapd = 0
+	try:
+		if not video_name.endswith('.gif'):
+			video_name += '.gif'
+		with open(file, 'rb') as csvfile:
+			reader = csv.reader(csvfile)
+			for row in reader:
+				lon = row[1]
+				lat = row[2]
+				versions = csv.reader(StringIO(row[3][1:-1]))
+				for row in versions:
+					if len(row) == 2:
+						stamp = row[1]
+						parsd += 1
+						if parsd % 10000 == 0:
+							print("num nodes parsed: ", parsd)
+						add_node(lon, lat, stamp)
+		csvfile.close()
+		with imageio.get_writer(video_name, mode='I') as writer:
+			for stamp in days:
+				# make the map
+				_map = StaticMap(width, height, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
+				# add the nodes
+				for node in days[stamp].nodes:
+					_map.add_marker(node)
+				# add the ways
+				for way in days[stamp].ways:
+					_map.add_polygon(way)
+				# render
+				_img = _map.render(zoom=zoom)
+				# save
+				_name = stamp + ".png"
+				_img.save(_name)
+				# now add to frames list for video
+				frame = imageio.imread(_name)
+				writer.append_data(frame)
+				mapd += 1
+				if mapd % 1000 == 0:
+					print("num nodes mapped into video: ", mapd)
+			# now exit gracefully
+			print("all done.")
+	except:
+		PrintException()
+
+# test
+read_csv('/Users/maxvonhippel/Documents/OSMHistoryServer/nodes.csv', 500, 500, 5, "nepal-osm-vid")
